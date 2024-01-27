52044984a367a966f300b4186a2808cd53b80355
Jin Zhang <JinLZhang278@hotmail.com>
2024-01-27 01:16:07 -0500

    Updated Selenium

--- scraper.py
+++ scraper.py
@@ -1,20 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import time
 
 if __name__ == "__main__":
-	url = "https://app.sensortower.com/ios/rankings/top/ipad/us/all-categories?date=2022-10-13"
-	driver = webdriver.Chrome(executable_path=r"C:\Users\Jin\Documents\GitHub\IosStoreScraper\chromedriver.exe") 
-	driver.get(url)
+	url = "https://noblemtl.com/childhood-friend-of-the-zenith-chapter-198/"
 	
-	time.sleep(10)
+	page_to_scrape = webdriver.Chrome()
+	page_to_scrape.get(url)
+	time.sleep(20)
+	# page = driver.page_source
+	# soup = BeautifulSoup(page, "html.parser")
 
-	page = driver.page_source
-	soup = BeautifulSoup(page, "html.parser")
-
-	all_divs = soup.find('tbody', {'class' : 'rankings-app-body'})
-	game_profiles = all_divs.find_all('div', {'class' : 'regular-info-container'})
-	print(game_profiles)
+	# all_divs = soup.find('tbody', {'class' : 'rankings-app-body'})
+	# game_profiles = all_divs.find_all('div', {'class' : 'regular-info-container'})
+	# print(game_profiles)
